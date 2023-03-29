d0b0e7062998f0a462ca4ec2cc02f6bfa43c1256
mFaridun <musirmonovfaridun@gmail.com>
2023-03-30 01:30:04 +0500

    function  declaration, expression, Arrow

--- main.js
+++ main.js
@@ -23,24 +23,63 @@
 
 // switch case
 
-const haftaKuni = Number(prompt('Bugin haftaning qaysi kuni'));
+// const haftaKuni = Number(prompt('Bugin haftaning qaysi kuni'));
 
-switch (haftaKuni) {
-  case 1:
-    console.log('Dushanba'); break;
-  case 2: 
-    console.log('Seshanba'); break;
-  case 3:
-    console.log('Chorshanba'); break;
-  case 4:
-    console.log('Payshanba'); break;
-  case 5:
-    console.log('Juma'); break;
-  case 6:
-    console.log('Shanba'); break;
-  case 7:
-    console.log('Yakshanba'); break;
+// switch (haftaKuni) {
+//   case 1:
+//     console.log('Dushanba'); break;
+//   case 2:
+//     console.log('Seshanba'); break;
+//   case 3:
+//     console.log('Chorshanba'); break;
+//   case 4:
+//     console.log('Payshanba'); break;
+//   case 5:
+//     console.log('Juma'); break;
+//   case 6:
+//     console.log('Shanba'); break;
+//   case 7:
+//     console.log('Yakshanba'); break;
 
-  default:
-    console.log('Bunaqa hafta kuni mavjud emas'); break
+//   default:
+//     console.log('Bunaqa hafta kuni mavjud emas'); break
+// }
+
+// function declaration
+// function juftToq(element) {
+
+//   if (element % 2 === 0) {
+//     console.log(`${element} juft son.`);
+//   }
+//   else {
+//     console.log(`${element} juft son emas.`);
+//   }
+// }
+
+// juftToq(4);
+
+
+// function expression
+// const juftToq = function(element) {
+//     if (element % 2 === 0) {
+//     console.log(`${element} juft son.`);
+//   }
+//   else {
+//     console.log(`${element} juft son emas.`);
+//   }
+// }
+
+// juftToq(8)
+
+
+// Arrow function
+const juftToq = element => {
+    if (element % 2 === 0) {
+    console.log(`${element} juft son.`);
+  }
+  else {
+    console.log(`${element} juft son emas.`);
+  }
 }
+
+juftToq(9);
